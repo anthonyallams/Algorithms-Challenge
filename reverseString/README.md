901d685b01f3569a78f6b8169954217739b2c401
Allams <anthony.allams@gmail.com>
2020-04-01 11:24:55 +0100

    Solution 1-Using for loop to reverse String

---
+++ reverseString/README.md
@@ -0,0 +1,8 @@
+# CHALLENGE
+
+    Given a string, return a new string with the reversed order of character
+
+## EXAMPLE
+
+- reverseString('apple') ===> 'leppa'
+- reverseString('hello') ===> 'olleh'
---
+++ reverseString/reverseString.js
@@ -0,0 +1,10 @@
+//SOLUTION 1: Using for loop
+const reverseString = str => {
+  let reversedString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i];
+  }
+  return reversedString;
+};
+
+reverseString("apple");
