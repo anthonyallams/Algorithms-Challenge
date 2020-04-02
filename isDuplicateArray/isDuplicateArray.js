716fba17584650ed6c3be0e58421e1585711715d
Allams <anthony.allams@gmail.com>
2020-04-02 10:28:57 +0100

    Solution 1-Using for loop, indexOf and lastIndexOf of values

--- isDuplicateArray/isDuplicateArray.js
+++ isDuplicateArray/isDuplicateArray.js
@@ -1,1 +1,9 @@
-
+// SOLUTION 1: Using for loop
+const isDuplicateArray = array => {
+  for (let i = 0; i < array.length; i++) {
+    if (array.lastIndexOf(array[i]) !== array.indexOf(array[i])) {
+      return true;
+    }
+  }
+  return false;
+};
