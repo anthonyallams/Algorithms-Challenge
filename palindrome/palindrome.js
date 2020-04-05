2685ab701d14acba6055f9e36861fd1ce07fe702
Allams <anthony.allams@gmail.com>
2020-04-05 08:12:36 +0100

    Solution 2: Using the every function to compare initial and last characters

--- palindrome/palindrome.js
+++ palindrome/palindrome.js
@@ -1,5 +1,15 @@
 // SOLUTION 1: Using basic string methods
 const palindrome = (str) => {
-  // Convert str to array, reverse, join and compare the result to original string
+  // Convert str to array, reverse, join and compare the result to original string, then return a boolean
   return str.split("").reverse().join("") === str;
+};
+
+// SOLUTION 2: Using every function
+const palindrome = (str) => {
+  //Convert string to array, run every method on it
+
+  return str.split("").every((char, index) => {
+    // Check if the first character is equivalent to the last character and vice versa. Return a boolean
+    return char === str[str.length - i - 1];
+  });
 };
