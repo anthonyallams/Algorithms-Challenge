f7348d08195a3f456fb841351981a28b5a37be0d
Allams <anthony.allams@gmail.com>
2020-04-05 10:08:25 +0100

    Solution 1: Using string, array and Number methods to reverse integer

--- README.md
+++
@@ -1,3 +0,0 @@
-# Algorithms and Data Struture Challenge
-
-    This repo is for daily Algorithm and data structure challenges.
--- reverseInt/reverseInt.js
+++ reverseInt/reverseInt.js
@@ -1,1 +1,8 @@
+// SOLUTION 1: Using string and array methods
 
+const reverseInt = (int) =>
+  // Convert integer to string and then to array using split
+  // Reverse the array and convert back to string using join
+  // Convert string to integer using parseInt
+  // Check for +ve or -ve values using Math.sign
+  parseInt(int.toString().split("").reverse().join("")) * Math.sign(int);
