b852b1d53b7d142a559e6f8ef665bd3dbc6981c0
Allams <anthony.allams@gmail.com>
2020-04-07 13:41:00 +0100

    Solution 2: Using for of loop

--- vowelCount/vowelCount.js
+++ vowelCount/vowelCount.js
@@ -10,3 +10,19 @@ const vowelCount = (str) => {
     return acc;
   }, 0);
 };
+
+// SOLUTION 2: Using for of loop to loop through str and compare with vowels to get count
+const vowelCount = (str) => {
+  const strArray = str.toLowerCase().split("");
+  const vowels = ["a", "e", "i", "o", "u"];
+  let count = 0;
+
+  //Iterate through the string array
+  for (let str of strArray) {
+    //If char in the array is contained in the vowels array, increment
+    if (vowels.includes(str)) {
+      count++;
+    }
+  }
+  return count;
+};
