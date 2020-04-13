c9dca343c5ba4fdd105068ff09e71ee3fe054e02
Allams <anthony.allams@gmail.com>
2020-04-13 12:29:17 +0100

    Solution 1 using Iterative for loop approach

---
+++ sumOddFibonacciNums/README.md
@@ -0,0 +1,10 @@
+# CHALLENGE
+
+    Given a positive integer n, return the sum of odd fibonacci numbers that are less than or equal to num.
+    Example, sumFibs(10) should return 10 because all odd fibonacci numbers less than or equal to 10 are 1,1,3 and 5
+
+## EXAMPLE
+
+    sumOddFibonacciNums(10) ==> 10
+    sumOddFibonacciNums(1000) ==> 1785
+    sumOddFibonacciNums(4000000) ==> 4613732
---
+++ sumOddFibonacciNums/sumOddFibonacciNums.js
@@ -0,0 +1,25 @@
+// SOLUTION 1: Using for loop Iterative approach
+const sumOddFibonacciNums = (n) => {
+  let sumOdds = 0;
+  const results = [0, 1];
+
+  // Loop through from 2 to n and add the prev and current values to results
+  for (let i = 2; i <= n; i++) {
+    const a = results[i - 1];
+    const b = results[i - 2];
+
+    results.push(a + b);
+  }
+
+  // Loop through result array. Check if each value is odd and less than n
+  // Add the qualified values to sumOdds
+  results.forEach((result) => {
+    if (result % 2 !== 0 && result <= n) {
+      sumOdds += result;
+    }
+  });
+  return sumOdds;
+};
+
+
+};
