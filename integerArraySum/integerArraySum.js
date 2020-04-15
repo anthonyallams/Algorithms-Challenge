ada1bedbcea6b0beb687bbdad827755f6dd61227
Allams <anthony.allams@gmail.com>
2020-04-15 11:04:45 +0100

    Solution 1 using reduce helper and rest operator

--- integerArraySum/integerArraySum.js
+++ integerArraySum/integerArraySum.js
@@ -1,1 +1,16 @@
+// SOLUTION: Using reduce helper and rest operator
 
+const integerArraySum = (...nums) => {
+  // Check if index 0 is array or ineger
+  // If array, flatten the whole array and sum using reduce
+  if (Array.isArray(nums[0])) {
+    let numArr = nums.flat();
+    return numArr.reduce((acc, num) => {
+      return acc + num;
+    }, 0);
+  } else {
+    return nums.reduce((acc, num) => {
+      return acc + num;
+    }, 0);
+  }
+};
