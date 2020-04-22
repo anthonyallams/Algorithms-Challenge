072a6a519b71158cba45c439023e194720510503
Allams <anthony.allams@gmail.com>
2020-04-22 13:05:08 +0100

    Solution 1- Using reduce method

--- arrayReplace/arrayReplace.js
+++ arrayReplace/arrayReplace.js
@@ -1,7 +1,10 @@
 // SOLUTION 1: Using reduce method
 const arrayReplace = (inputArray, elemToReplace, substitutionElem) => {
   return inputArray.reduce((acc, arr) => {
-    arr === elemToReplace ? arr === substitutionElem : arr;
+    arr === elemToReplace ? (arr = substitutionElem) : arr;
+    // if (arr === elemToReplace) {
+    //   arr = substitutionElem;
+    // }
     return acc.concat(arr);
   }, []);
 };
