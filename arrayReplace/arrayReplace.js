8f106456c8fea873acd383a52f110ca8ea5ae221
Allams <anthony.allams@gmail.com>
2020-04-22 13:19:11 +0100

    Solution 3: Using for of loop and modifying the inputArray

--- arrayReplace/arrayReplace.js
+++ arrayReplace/arrayReplace.js
@@ -9,12 +9,22 @@ const arrayReplace = (inputArray, elemToReplace, substitutionElem) => {
   }, []);
 };
 
-// SOLUTION 2: Using map method
+// SOLUTION 2: Using map method and modifying inputArray
 const arrayReplace2 = (inputArray, elemToReplace, substitutionElem) => {
   inputArray.map((arr, index) => {
     arr === elemToReplace
       ? (inputArray[index] = substitutionElem)
       : inputArray[index];
   });
+  return inputArray;
+};
+
+// SOLUTION 3: Using for of loop and modifying the inputArray
+const arrayReplace2 = (inputArray, elemToReplace, substitutionElem) => {
+  for (let [i, v] of inputArray.entries()) {
+    inputArray[i] === elemToReplace
+      ? (inputArray[i] = substitutionElem)
+      : inputArray[i];
+  }
   return inputArray;
 };
